38c98e25134357a395955b250775fb7dccf081ac
Alina <alinamardanova5@gmail.com>
2025-01-22 00:12:05 +0300

    first comment on branch

---
+++ src/HelloJava.java
@@ -0,0 +1,18 @@
+public class BamkAccount {
+    void popolnenieScheta (int a) {
+
+    }
+
+    public static void main (String[] args) {
+        HelloJava MyAccount= new HelloJava();
+        MyAccount.id=1;
+        System.out.println(MyAccount.id);
+        System.out.println("Получилось");
+
+
+
+
+
+
+    }
+}
